361710c2f03b4414bebcda2191e5a7d3ebf1257b
gavin-go-dang <gavin.dang.goldenowl@gmail.com>
2023-09-25 13:47:29 +0700

    fix: api_urls file

--- config/api_urls.py
+++ config/api_urls.py
@@ -1,7 +1,5 @@
 from django.urls import include, path
 
-app_name = "api"
-
 urlpatterns = [
     path("users/", include("helpbot_backend.users.urls", namespace="users")),
     path("manage-project/", include("helpbot_backend.manage_project.urls", namespace="manage_project")),
