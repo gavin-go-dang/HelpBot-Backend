e03cc6d79333c36c8f354134191392338f52c43f
gavin-go-dang <gavin.dang.goldenowl@gmail.com>
2023-09-25 11:11:31 +0700

    fix: fix url path

--- helpbot_backend/users/urls.py
+++ helpbot_backend/users/urls.py
@@ -6,13 +6,12 @@ from helpbot_backend.users.views import ClerkCreateUser, CreateUser
 
 app_name = "users"
 urlpatterns = [
-    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("api/clerk-create/", ClerkCreateUser.as_view(), name="clerk-create"),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("clerk-create/", ClerkCreateUser.as_view(), name="clerk-create"),
 ]
 
 
 router = DefaultRouter()
-router.register("create", CreateUser, basename="create")
-# router.register("clerk-create", ClerkCreateUser, basename="clerk_create")
+router.register("v1/create", CreateUser, basename="create")
 urlpatterns += router.urls
