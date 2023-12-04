b57168e36be254046fed0b32a27750a5f2f41cc4
gavin-go-dang <gavin.dang.goldenowl@gmail.com>
2023-12-04 11:58:56 +0700

    fix: style conversation widget api

--- helpbot_backend/manage_project/views/get_style_conversation.py
+++ helpbot_backend/manage_project/views/get_style_conversation.py
@@ -2,10 +2,11 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from ..models import Conversation
+from ..serializers import ProjectSerializers
 
 
 class StyleConversation(APIView):
     def get(self, request, id, *args, **kwargs):
-        # breakpoint()
-        style = Conversation.objects.get(idx=id).project.style_widget
-        return Response(style)
+        project = Conversation.objects.get(idx=id).project
+        serializer = ProjectSerializers(project)
+        return Response(serializer.data)
