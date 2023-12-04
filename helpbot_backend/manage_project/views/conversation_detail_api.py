355b9e832b0275906d94ef62a3b6c123338a9f9a
gavin-go-dang <gavin.dang.goldenowl@gmail.com>
2023-12-04 17:47:03 +0700

    fix: api list conversation

--- helpbot_backend/manage_project/views/conversation_detail_api.py
+++ helpbot_backend/manage_project/views/conversation_detail_api.py
@@ -3,15 +3,16 @@ from django.db.models import Min
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from ..models import Message, Room
+from ..models import Conversation, Message
 
 
 class CombinedInfoAPIView(APIView):
     def get(self, request, id, *args, **kwargs):
-        rooms = Room.objects.filter(project__id=id).values("id_synapse")
+        # breakpoint()
+        conversation = Conversation.objects.filter(project__id=id).values("idx")
         data = (
             Message.objects.exclude(conversation="")
-            .filter(room__in=rooms)
+            .filter(conversation__in=conversation)
             .values("conversation")
             .distinct()
             .annotate(earliest_time=Min("created_at"))
